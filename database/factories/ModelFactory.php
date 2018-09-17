9939ffea8152c7cd788b58f4f1e34cfd1f5370ae
BenjaMolina <molinasb.94@gmail.com>
2018-09-16 22:54:26 -0500

    Creando Factories - Transaction

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -3,6 +3,8 @@
 use App\User;
 use App\Category;
 use App\Product;
+use App\Transaction;
+use App\Seller;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,6 +53,20 @@ $factory->define(Product::class, function (Faker\Generator $faker) {
         'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
         'seller_id' => User::inRandomOrder()->first()->id,
         // 'seller_id' => User::all()->random()->id,        
+    ];
+});
+
+$factory->define(Transaction::class, function (Faker\Generator $faker) {
+
+    $vendedor = Seller::has('products')->get()->random();
+    $comprador = User::all()->except($vendedor->id)->random();
+   
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'buyer_id' => $comprador->id,
+        'product_id' => $vendedor->products->random()->id,
     ];
 });
 
