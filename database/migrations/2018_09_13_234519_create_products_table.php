b758dfd9acb81880e8b8d2ef9eba3c7b0bf9bc1e
BenjaMolina <molinasb.94@gmail.com>
2018-09-16 20:24:30 -0500

    Estrutura de la BD usando Migraciones - Migraciones Categories

--- database/migrations/2018_09_13_234519_create_products_table.php
+++ database/migrations/2018_09_13_234519_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->integer('quantity')->unsigned();
             $table->string('status')->default(Product::PRODUCT_DISPONIBLE);
             $table->string('image');
