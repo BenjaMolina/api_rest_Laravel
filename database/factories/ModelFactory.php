6262192461395ab2532cacef29bdfb87014ab4ff
BenjaMolina <molinasb.94@gmail.com>
2018-09-16 22:45:01 -0500

    Creando Factories - Product

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -2,6 +2,7 @@
 
 use App\User;
 use App\Category;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +38,19 @@ $factory->define(Category::class, function (Faker\Generator $faker) {
         'name' => $faker->word,
         'description' => $faker->paragraph(1),
         
+    ];
+});
+
+$factory->define(Product::class, function (Faker\Generator $faker) {
+   
+    return [
+        'name' => $faker->word,
+        'description' => $faker->paragraph(1),
+        'quantity' => $faker->numberBetween(1,10),
+        'status' => $faker->randomElement([Product::PRODUCT_DISPONIBLE,Product::PRODUCT_NO_DISPONIBLE]),
+        'image' => $faker->randomElement(['1.jpg','2.jpg','3.jpg']),
+        'seller_id' => User::inRandomOrder()->first()->id,
+        // 'seller_id' => User::all()->random()->id,        
     ];
 });
 
