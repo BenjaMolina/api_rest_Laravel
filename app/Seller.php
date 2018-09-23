f0d3fa12cdcad4e75652352970433f50660dbb76
BenjaMolina <molinasb.94@gmail.com>
2018-09-22 23:21:07 -0500

    Inyeccion implicita de Modelos - Resolviendo Seller y Global Scopes

--- app/Seller.php
+++ app/Seller.php
@@ -5,9 +5,18 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 use App\Product;
+use App\Scopes\SellerScope;
 
 class Seller extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new SellerScope);
+    }
+
+    
     public function products()
     {
         return $this->hasMany(Product::class);
