043f96c6388ac9716053e86989e235f717adfb5f
BenjaMolina <molinasb.94@gmail.com>
2018-10-25 23:55:23 -0500

    Operaciones complejas Product - ProdcutBuyerTransactionController

---
+++ app/Http/Controllers/Product/ProductBuyerTransactionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Product;
+use App\User;
+use App\Transaction;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\DB;
+
+class ProductBuyerTransactionController extends ApiController
+{
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Product $product, User $buyer)
+    {
+        $this->validate($request,[
+            'quantity' => 'required|integer|min:1',
+        ]);
+
+        if($buyer->id == $product->seller_id)
+        {
+            return $this->errorResponse('El comprador debe ser diferente al vendedor', 409);
+        }
+
+        if(!$buyer->esVerficado())
+        {
+            return $this->errorResponse('El comprador debe ser un usuario verificado', 409);
+        }
+        if(!$product->seller->esVerficado())
+        { 
+            return $this->errorResponse('El vendedor debe ser un usuario verificado', 409);
+        }
+        if(!$product->estaDisponible())
+        {
+            return $this->errorResponse('El producto para esta transaccion no esta disponible', 409);
+        }
+        if($product->quantity < $request->quantity)
+        {
+            return $this->errorResponse('El producto no tiene la cantidad disponible requerida para esta transaccion', 409);
+        }
+
+
+        return DB::transaction(function() use ($request, $product, $buyer){
+            
+            $product->quantity -= $request->quantity;
+            $product->save();
+
+            $transaction = Transaction::create([
+                'quantity' => $request->quantity,
+                'buyer_id' => $buyer->id,
+                'product_id' => $product->id,
+            ]);
+
+            return $this->showOne($transaction, 201);
+        });
+
+    }
+}
