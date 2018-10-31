f38a7b3eddfa0bcb30adcce44aa33f98e9af64f6
BenjaMolina <molinasb.94@gmail.com>
2018-10-30 22:40:52 -0600

    Middleware pesonalizados - Limitador de peticiones

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -25,38 +25,38 @@ class Kernel extends HttpKernel
      *
      * @var array
      */
-    protected $middlewareGroups = [
-        'web' => [
-            \App\Http\Middleware\EncryptCookies::class,
-            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
-            \Illuminate\Session\Middleware\StartSession::class,
-            // \Illuminate\Session\Middleware\AuthenticateSession::class,
-            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        protected $middlewareGroups = [
+            'web' => [
+                \App\Http\Middleware\EncryptCookies::class,
+                \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+                \Illuminate\Session\Middleware\StartSession::class,
+                // \Illuminate\Session\Middleware\AuthenticateSession::class,
+                \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+                \App\Http\Middleware\VerifyCsrfToken::class,
+                \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            ],
 
-        'api' => [
-            'signature:X-Aplication-Name',
-            'throttle:60,1',
-            'bindings',
-        ],
-    ];
+            'api' => [
+                'signature:X-Aplication-Name',
+                'throttle:5,1',
+                'bindings',
+            ],
+        ];
 
-    /**
-     * The application's route middleware.
-     *
-     * These middleware may be assigned to groups or used individually.
-     *
-     * @var array
-     */
-    protected $routeMiddleware = [
-        'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
-        'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
-        'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        'can' => \Illuminate\Auth\Middleware\Authorize::class,
-        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'signature' => \App\Http\Middleware\SignatureMiddleware::class,
-    ];
+        /**
+         * The application's route middleware.
+         *
+         * These middleware may be assigned to groups or used individually.
+         *
+         * @var array
+         */
+        protected $routeMiddleware = [
+            'auth' => \Illuminate\Auth\Middleware\Authenticate::class,
+            'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
+            'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            'can' => \Illuminate\Auth\Middleware\Authorize::class,
+            'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+            'throttle' => \App\Http\Middleware\LimitadorMiddleware::class,
+            'signature' => \App\Http\Middleware\SignatureMiddleware::class,
+        ];
 }
