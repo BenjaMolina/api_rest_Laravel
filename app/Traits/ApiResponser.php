0ae2ed55eae57de01245b71e9aab633d9b65d66b
BenjaMolina <molinasb.94@gmail.com>
2018-11-07 22:18:49 -0600

    Paginacion - Tamaño de pagina personalizada

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -4,6 +4,7 @@ namespace App\Traits;
 
 use Illuminate\Support\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 trait ApiResponser
@@ -95,11 +96,18 @@ trait ApiResponser
 
     protected function paginate(Collection $collection)
     {
+        Validator::validate(request()->all(),[
+            'per_page' => 'integer|min:2|max:50'
+        ]);
+
         //Resuelve la pagina que nos encontramos (actual)
         $page = LengthAwarePaginator::resolveCurrentPage();
         
         //Numero de elementos por pagina
         $perPage = 15;
+        if(request()->has('per_page')){
+            $perPage = (int) request()->per_page;
+        }
         //Dividimos la coleccion ( inicio - fin ) 
         $results = $collection->slice(( $page - 1) * $perPage,$perPage)->values();
         
